27eabb36847667403990793e3d9043ee305650a0
Walterwilly <shanekk2401@gmail.com>
2025-04-10 15:02:05 +0700

    remove unnessesary

---
+++ src/app.js
@@ -0,0 +1,43 @@
+// src/app.js
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connect from './config/mongodb.js';
+import authenticationRoute from './router/authenticationRoute.js';
+import apiRoute from './router/apiRoute.js';
+
+const createApp = () => {
+  const app = express();
+
+  const corsOption = {
+    origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST'],
+  };
+
+  app.use(cors(corsOption));
+  app.options('*', cors(corsOption));
+  app.use(express.json());
+  app.use(cookieParser());
+
+  app.use('/auth', authenticationRoute);
+  app.use('/api', apiRoute);
+
+  return app;
+};
+
+const startServer = async () => {
+  const app = createApp();
+  const PORT = process.env.PORT || 5000;
+
+  try {
+    await connect();
+    app.listen(PORT, () => {
+      console.log(`✅ Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('❌ Failed to start server:', error);
+  }
+};
+
+export default startServer;
--- src/router/apiRoute.js
+++ src/router/apiRoute.js
@@ -2,12 +2,10 @@ import express from "express";
 import authMiddleware from "../authMiddleware.js";
 import { sayHello } from "../controller/helloController.js";
 import { getUser , getUerChats} from "../controller/userController.js"
-import ask from "../controller/chat.js";
 const router = express.Router()
 
 router.get('/hello', authMiddleware, sayHello)
 router.get('/user', authMiddleware, getUser)
 router.get('/chats', authMiddleware, getUerChats)
-router.post('/ask', authMiddleware, ask)
 
 export default router;
