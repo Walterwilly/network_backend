f60fad094e5a67144161ff715b53cb7c70ca25ac
Walterwilly <shanekk2401@gmail.com>
2025-04-10 18:28:11 +0700

    socket is on the same port as server

--- src/app.js
+++ src/app.js
@@ -31,16 +31,12 @@ const createApp = () => {
 const startServer = async () => {
   const app = createApp();
   const PORT = process.env.PORT || 5000;
-  const server = http.createServer(app);
   try {
     await connect();
-    await socketHandler(server);
-    app.listen(PORT, () => {
+    const server = app.listen(PORT, () => {
       console.log(`✅ Server running on http://localhost:${PORT}`);
     });
-    server.listen(888, () => {
-        console.log(`✅ Server running on http://localhost:${888}`);
-    });
+    socketHandler(server)
   } catch (error) {
     console.error('❌ Failed to start server:', error);
   }
